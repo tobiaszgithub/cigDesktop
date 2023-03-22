e6f4781e2846dd1af2e708a1336d2b4cbffeb25c
Tobiasz Hołtyn <tobiasz.github@gmail.com>
2023-03-22 09:44:45 +0100

    update wails dependencies and maximise on start up

--- main.go
+++ main.go
@@ -17,9 +17,10 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "cigDesktop",
-		Width:  1024,
-		Height: 768,
+		Title:            "cigDesktop",
+		Width:            1024,
+		Height:           768,
+		WindowStartState: options.Maximised,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
